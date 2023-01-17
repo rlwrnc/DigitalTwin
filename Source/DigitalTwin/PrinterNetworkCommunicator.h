f6492c122916cdc4f32e86edf83901bedbe7c58f
Lawrence <rllawr04@louisville.edu>
2023-01-17 16:07:44 -0500

    attempting to fix model units and beginning printer movement code

--- Source/DigitalTwin/PrinterNetworkCommunicator.h
+++ Source/DigitalTwin/PrinterNetworkCommunicator.h
@@ -22,7 +22,9 @@ public:
 	UPrinterNetworkCommunicator();
 
 protected:
-	UTextRenderComponent *TemperatureText;
+	UTextRenderComponent* TemperatureText;
+	AActor xAxis, yAxis, zAxis;
+	bool isRunningJob;
 	double toolTemp, bedTemp;
 	FTimerHandle TemperatureHandle;
 
@@ -35,5 +37,4 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr response, bool success);
-		
 };
